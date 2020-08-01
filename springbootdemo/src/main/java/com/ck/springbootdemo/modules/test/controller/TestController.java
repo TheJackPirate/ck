9dde3ed90c164e6a3d9d46c34952c899451f69d6
TheJackPirate <1539425803@qq.com>
2020-08-01 10:53:23 +0800

    "页面数据处理"

--- springbootdemo/src/main/java/com/ck/springbootdemo/modules/test/controller/TestController.java
+++ springbootdemo/src/main/java/com/ck/springbootdemo/modules/test/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.ck.springbootdemo.modules.test.controller;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ck.springbootdemo.modules.test.entity.City;
+import com.ck.springbootdemo.modules.test.entity.Country;
+import com.ck.springbootdemo.modules.test.service.CityService;
+import com.ck.springbootdemo.modules.test.service.CountryService;
 import com.ck.springbootdemo.modules.test.vo.ApplicationTest;
 
 @Controller
@@ -31,8 +39,28 @@ public class TestController {
 	@Autowired
 	private ApplicationTest applicationTest;
 	
+	@Autowired
+	private CityService cityService;
+	@Autowired
+	private CountryService countryService;
+	
 	@RequestMapping("/index")
 	public String testIndex(ModelMap modelMap){
+		int countryId = 522;
+		Country country = countryService.getCountryByCountryId(countryId);
+		List<City> cities = cityService.getCitiesByCountryId(countryId);
+		
+		modelMap.addAttribute("thymeleafTitle", "Title");
+		modelMap.addAttribute("checked", true);
+		modelMap.addAttribute("currentNumber", 99);
+		modelMap.addAttribute("changeType", "checkbox");
+		modelMap.addAttribute("baiduUrl", "/baidu");
+		modelMap.addAttribute("city", cities.get(0));
+		modelMap.addAttribute("shopLogo", "https://cdn.duitang.com/uploads/item/201308/13/20130813115619_EJCWm.thumb.700_0.jpeg");
+		modelMap.addAttribute("country", country);
+		modelMap.addAttribute("cities", cities.stream().limit(10).collect(Collectors.toList()));
+		modelMap.addAttribute("updateCityUri", "/api/city");
+		
 		//将我们真正需要展示的页面路径包装到template属性中，由碎片组装器进行组装
 		modelMap.addAttribute("template","test/index");
 		//直接返回外层碎片组装器classpath:/templates/index.html
